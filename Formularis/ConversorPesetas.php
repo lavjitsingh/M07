1df626b2b94e491994e8715754c8d78831b3a4ec
Lavjit Singh <ls2638@cirvianum.cat>
2017-11-02 17:05:53 +0100

    Conversor de monedes

---
+++ Formularis/ConversorPesetas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Conversor</title>
+    </head>
+
+    <body>
+        
+        <h3>Conversor de euros a pesetas</h3>
+
+        <?php
+       
+        ?>
+
+    </body>
+
+</html>
